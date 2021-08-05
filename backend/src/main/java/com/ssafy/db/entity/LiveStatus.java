45619288f919ae45fd41f24013e2f0dc187fe78b
yjanmin <yjanmin@naver.com>
2021-08-05 13:52:02 +0900

    [fix] create live error

--- backend/src/main/java/com/ssafy/db/entity/LiveStatus.java
+++ backend/src/main/java/com/ssafy/db/entity/LiveStatus.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,7 +19,7 @@ public class LiveStatus { //라이브 상태 테이블
     String liveStatusName; //라이브 상태 이름
 
     //mappedBy - 자신을 참조하고 있는 애들
-    @OneToOne(mappedBy = "liveStatus") //Live테이블과 1:1관계
-    private Live live;
+    @OneToMany(mappedBy = "liveStatus") //Live테이블과 1:1관계
+    private List<Live> livelist;
 
 }

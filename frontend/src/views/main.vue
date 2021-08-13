f2ae08758b995dcd2079d0b6a2970539a6e3e58e
ssw02238 <ssw0223820@gmail.com>
2021-08-13 18:32:24 +0900

    develop 1.1

--- frontend/src/views/main.vue
+++ frontend/src/views/main.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="font-family: 'InfinitySans-RegularA1';">
-    <!-- carousel -->
+    <!-- carousel-->
     <v-carousel
       :continuous="false"
       :show-arrows="false"

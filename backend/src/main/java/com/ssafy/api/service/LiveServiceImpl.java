2af70d0f536367bcee87f38b46c93210c004d2eb
jaewon <jwsims@naver.com>
2021-08-16 18:29:54 +0900

    [chore] merge

--- backend/src/main/java/com/ssafy/api/service/LiveServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/LiveServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 

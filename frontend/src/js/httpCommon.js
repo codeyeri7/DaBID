491a7ba3816d473dbe21bc9e992be0f621613ba6
ssw02238 <ssw0223820@gmail.com>
2021-08-19 15:11:53 +0900

    http]

--- frontend/src/js/httpCommon.js
+++ frontend/src/js/httpCommon.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default {
     axios: axios.create({
-        baseURL: "https://i5a506.p.ssafy.io/",
+        baseURL: "https://i5a506.p.ssafy.io/api",
         // baseURL: "https://localhost:8080/",
         headers: {
             "Content-Type": "application/json",
